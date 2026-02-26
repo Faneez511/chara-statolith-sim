6792825ea7b224461857c3aca7bf90121b2556dd
Faneez Polat <faneezpolat@gmail.com>
2026-02-26 01:57:10 +0100

    tuned parameters

--- physics/forces.py
+++ physics/forces.py
@@ -18,31 +18,27 @@ def compute_forces_single(s, params):
     gx = np.cos(winkel2) * np.cos(winkel1)
     gy = np.cos(winkel2) * np.sin(winkel1)
     gz = np.sin(winkel2)
-    g_vec = np.array([gx, gy, gz]) * 0
+    g_vec = np.array([gx, gy, gz])
 
     dp = (p_stato - params.p_cyto) * 1e-12
     F_grav_mag = (4/3) * np.pi * r**3 * dp * params.g_mag
     v_sed_vec = F_grav_mag * mobility * g_vec 
 
-    shield = 1.0
-    if params.ACTIN_MIN_X <= x <= params.ACTIN_MAX_X:
-        shield = 0.3
-
-    v_total = v_sed_vec * shield
+    v_total = v_sed_vec
 
     x_mid = 0.5 * (params.ACTIN_MIN_X + params.ACTIN_MAX_X)
     dx = x - x_mid
-    k_actin = 2e-10
-    F_actin_x = -k_actin * dx
+    k_actin = 1e-14
+    F_actin_x = -k_actin * dx 
 
     v_total[0] += F_actin_x * mobility
 
     # Mittelpunkt der gravisensitiven Zone
     center = np.array([32.5, 0.0, 0.0])
     r_vec = s[0:3] - center
-    F_center_strength = 0.03
+    F_center_strength = 0.1e-4
     F_center = -F_center_strength * r_vec
-    v_total += F_center
+    v_total += F_center * mobility
 
     return v_total
 
